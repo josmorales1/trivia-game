1eb16f2048724c55c4dd59a2f0d05f5fca39ca10
josmorales1 <josgerardomorales1@aol.com>
2019-10-12 23:52:57 -0500

    change a few things on my code in js

--- assets/js/app.js
+++ assets/js/app.js
@@ -121,8 +121,7 @@ $(document).ready(function(){
           .html('<h3>Thank you for playing!</h3>'+
           '<p>Correct: '+ trivia.correct +'</p>'+
           '<p>Incorrect: '+ trivia.incorrect +'</p>'+
-          '<p>Unaswered: '+ trivia.unanswered +'</p>'+
-          '<p>Please play again!</p>');
+          '<p>Press "Start Game" to play again!</p>');
         
         // hide game sction
         $('#game').hide();
